b2a09a5c0f64b55184e9d70dff03c119320e184e
Stanislav Kaisin <stanislavkaisin@MacBook-Air-Stanislav.local>
2021-09-07 20:17:20 +0300

    added db config

--- src/App/app.module.ts
+++ src/App/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'src/Config/config';
+
+console.log(`config`, config);
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(config.DB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/Config/config.ts
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+export const config = {
+  DB_URI: process.env.DB_URI,
+};
